1fc0747889fadfe979c3064cacf8ac350b163d7a
Po Huit <eve.po.huit@gmail.com>
2017-07-29 22:36:44 -0700

    got lines and points working reasonably

--- widget.py
+++ widget.py
@@ -15,35 +15,55 @@ import genstar
 class StarWidget:
     def __init__(self,  window):
         self.window = window
-        self.button = Button (window, text="Generate",
-                              command=self.do_generate)
-        self.button.pack()
         self.star = genstar.gen_star()
-        self.canvas = None
-        self.plot = None
+
+        self.use_lines = BooleanVar(window, True)
+        self.use_points = BooleanVar(window, False)
+
+        fig = Figure(figsize=(18,6))
+        self.plot = fig.add_subplot(1,1,1)
+        self.plot.set_title ("Star Luminance", fontsize=16)
+        self.plot.set_ylabel("%", fontsize=14)
+        self.plot.set_xlabel("day", fontsize=14)
+        self.canvas = FigureCanvasTkAgg(fig, master=self.window)
+        self.canvas.get_tk_widget().pack()
         self.plot_current()
+
+        buttons = Frame(window)
+        lines = Checkbutton(buttons, text="Lines",
+                            variable=self.use_lines,
+                            command=self.plot_current)
+        lines.pack(side=LEFT)
+        points = Checkbutton(buttons, text="Points",
+                             variable=self.use_points,
+                             command=self.plot_current)
+        points.pack(side=LEFT)
+        generate = Button(buttons, text="Generate",
+                          command=self.do_generate)
+        generate.pack(side=LEFT)
+        buttons.pack(fill=BOTH, expand=True)
 
     def do_generate(self):
         self.star = genstar.gen_star()
         self.plot_current()
 
     def plot_current(self):
-
-        if not self.canvas:
-            fig = Figure(figsize=(18,6))
-            self.plot = fig.add_subplot(1,1,1)
-            self.plot.set_title ("Star Luminance", fontsize=16)
-            self.plot.set_ylabel("%", fontsize=14)
-            self.plot.set_xlabel("day", fontsize=14)
-            self.canvas = FigureCanvasTkAgg(fig, master=self.window)
-            self.canvas.get_tk_widget().pack()
-
         spd = genstar.samples_per_day
         n = spd * genstar.days_per_trace
         x = [float(i) / spd for i in range(n)]
         y = [100.0 + s for s in self.star]
         self.plot.cla()
-        self.plot.scatter(x,y,color='blue')
+        style = 'b'
+        use_points = self.use_points.get()
+        use_lines = self.use_lines.get()
+        if use_points or use_lines:
+            if use_points:
+                style += 'o'
+            if use_lines:
+                style += '-'
+        else:
+            style += ':'
+        self.plot.plot(x, y, style)
         self.canvas.draw()
 
 window= Tk()
