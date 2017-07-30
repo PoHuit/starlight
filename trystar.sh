3ad665665e21c1a7ed3e2a403760cd1c0c1c6823
Po Huit <eve.po.huit@gmail.com>
2017-07-30 00:00:57 -0700

    removed now-superfluous trystar script

--- trystar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (c) 2017 Po Huit
-# [This program is licensed under the "MIT License"]
-# Please see the file COPYING in the source
-# distribution of this software for license terms.
-
-python3 genstar.py |
-gnuplot -p -e "plot '-' with linespoints;"
